dcbcab2f1321f0b33d9029318315f5fd396b2045
Veselin Valev <37142700+VeselinValev@users.noreply.github.com>
2019-01-09 20:33:34 +0200

    update

---
+++ NBA App -- React + Redux + Node + MongoDB/frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
+
+const App = () => {
+    return (
+        <BrowserRouter>
+
+        </BrowserRouter>
+    )
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
